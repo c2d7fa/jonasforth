32d0ec28e8ffda578fe8de8eef75426d8ef98f9c
Jonas Hvid <mail@johv.dk>
2019-11-14 18:47:05 +0100

    Implement basic return stack

--- main.asm
+++ main.asm
@@ -12,9 +12,29 @@ macro next {
     jmp qword [rax]         ; Jump to the codeword of the current word
 }
 
+;; pushr and popr work on the return stack, whose location is stored in the
+;; register RBP.
+macro pushr x {
+    sub rbp, 8
+    mov [rbp], x
+}
+macro popr x {
+    mov x, [rbp]
+    add rbp, 8
+}
+
 segment readable executable
 
 start:
+    ;; Initialize return stack
+    mov rbp, return_stack_top
+
     jmp $
 
 segment readable
+
+segment readable writable
+
+;; Return stack
+rq $2000
+return_stack_top:
